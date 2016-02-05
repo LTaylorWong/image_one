6b1d087b721ce55cbe1263a5a30edc4985cd82da
Liiiian <ltaylor.wong@gmail.com>
2016-02-04 21:07:27 -0500

    img

--- pic.c
+++ pic.c
@@ -5,27 +5,30 @@
 
 int main(){
 
-  int r,g,b,x,y, file;
-  char buff[100];
+  int r, g, b, x, y, n;
+  int file;
+  int xres, yres;
+  int max_c;
+  //char buff[100];
+  
   time_t;
-
   srand((unsigned) time(&t));
   n = rand() % 3
     
   r = 255;
-  x = 0;
-  y = 0;
+  xres = 100;
+  yres = 100;
   
   file = open("pic.ppm", 0_CREAT|0_WRONLY|0_RDONLY);
-
-  printf("P3 \n 100 100\n 255");
+  sprintf(header, "P3 \n %i,%i \n %i", xres, yres, max_c);
+  write(file, header, strlen(header));
   for (x<100; x++){
     for (y<100; y++){
-      x += n;
-      y += n;
-      sprintf()
-    write(file,line, strlen(line));
+      g += n;
+      b += n;
+      sprintf(line, "&d, &d, &d", r , g, b);
+      write(file, line, strlen(line));
+    }
   }
-  
   close(file);  
 }
